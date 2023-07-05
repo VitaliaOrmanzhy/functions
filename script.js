816ce164320ede62e942a8f9b2cde91e10b0cc3d
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-07-05 16:19:30 +0300

    another way to write a function

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function getNum(message) {
-    let num;
+function getNum(messages) {
 
-    do {
-        num = prompt(message);
+    
+    let nums = [];
 
-        if (num === null) {
-            alert('Жаль. Сподіваюсь ще побачитись');
-            break;
-        }
-        num = +num;
-    } while (typeof num !== 'number');
+    for (let i = 0; i < messages.length; i++) {
+        let num;
 
-    return num;
+        do {
+            let num = prompt(messages[i]);
+
+            if (num === null) {
+                alert('Жаль. Сподіваюсь ще побачитись');
+                break;
+            }
+
+            nums.push(+num);
+        } while (typeof num !== 'number');
+
+    }
+
+    return nums;
 }
+
+const arrOfMessages = [`Будемо знаходить рішення квадратного рівняння виду: ax^2 + bx + c = 0
+Введіть a`, `a = ${nums[0]}
+Введіть b`,`a = ${nums[0]}, b = ${nums[1]}
+Введіть c`]
 
 function getRoots(a, b, c) {
     
@@ -26,8 +39,8 @@ function getRoots(a, b, c) {
         return 'D < 0. Рівняння не має рішень';
     }
 
-    const x1 = (-b - Math.sqrt(D)) / 2 * a;
-    const x2 = (-b + Math.sqrt(D)) / 2 * a;
+    const x1 = (-b - Math.sqrt(D)) / (2 * a);
+    const x2 = (-b + Math.sqrt(D)) / (2 * a);
     
     if (D === 0) {
         return `D = 0. Єдиний корень рівняння: ${x1}`
@@ -36,11 +49,4 @@ function getRoots(a, b, c) {
     return `x1 = ${x1}, x2 = ${x2}`;
 }
 
-const a = getNum(`Будемо знаходить рішення квадратного рівняння виду: ax^2 + bx + c = 0
-Введіть a`);
-const b = getNum(`a = ${a}
-Введіть b`);
-const c = getNum(`a = ${a}, b = ${b}
-Введіть c`);
-
-alert(getRoots(a, b, c));
+alert(getRoots(...getNum(arrOfMessages)));
