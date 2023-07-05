2d5d586711a821cfcdbc7e06477ceff91b9e14f0
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-07-05 15:52:31 +0300

    the program is done

---
+++ script.js
@@ -0,0 +1,46 @@
+function getNum(message) {
+    let num;
+
+    do {
+        num = prompt(message);
+
+        if (num === null) {
+            alert('Жаль. Сподіваюсь ще побачитись');
+            break;
+        }
+        num = +num;
+    } while (typeof num !== 'number');
+
+    return num;
+}
+
+function getRoots(a, b, c) {
+    
+    if (a === 0) {
+        return 'a = 0. Рівняння не має рішень';
+    }
+
+    const D = b ** 2 - 4 * a * c;
+
+    if (D < 0) {
+        return 'D < 0. Рівняння не має рішень';
+    }
+
+    const x1 = (-b - Math.sqrt(D)) / 2 * a;
+    const x2 = (-b + Math.sqrt(D)) / 2 * a;
+    
+    if (D === 0) {
+        return `D = 0. Єдиний корень рівняння: ${x1}`
+    }
+
+    return `x1 = ${x1}, x2 = ${x2}`;
+}
+
+const a = getNum(`Будемо знаходить рішення квадратного рівняння виду: ax^2 + bx + c = 0
+Введіть a`);
+const b = getNum(`a = ${a}
+Введіть b`);
+const c = getNum(`a = ${a}, b = ${b}
+Введіть c`);
+
+alert(getRoots(a, b, c));
